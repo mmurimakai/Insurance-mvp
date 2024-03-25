020b99bca61cf50cdc9b73506c23233bd4aa955f
wbalter <wbanda@kabarak.ac.ke>
2024-03-25 19:16:07 +0000

    💻 Marketing necessities

--- routes/marketing.js
+++ routes/marketing.js
@@ -4,6 +4,10 @@ const router = express.Router();
 /// Home page route
 router.get("/:page", (req, res) => {
   const page = req.params.page;
+
+  // TODO: Fetch content based on page
+
+  // TODO: Markdown to html conversion
   return res.render("pages/marketing", { page });
 });
 
