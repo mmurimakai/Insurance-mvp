7acbf6660ef8756ebc5309c17893c5aad7243c6a
Dev Banda <wbanda@kabarak.ac.ke>
2024-03-25 19:48:37 +0000

    💻 Cobwebs cleaning plus alot of TODOS

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+// TODO: Build routes for sections
+// TODO: SSE setup
+
 /// Home page route
 router.get("/", (req, res) => {
   return res.render("pages/admin");
